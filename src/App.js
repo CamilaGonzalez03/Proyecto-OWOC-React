3321242d466faf6a9e52b3196bed02d6d3b01bae
Camila Gonzalez <camila.gonzalezcesar@gmail.com>
2022-09-19 19:44:03 -0300

    agrego archiv.js con prop greeting y styling integrado

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar';
+import ItemListContainer from './Components/ItemListContainer';
+import Productos from './Components/Productos';
 
 function App() {
   return (
     <>
-    <Navbar/>
+      <Navbar/>
       <h1 className='titulo' >OWOC Almacen Natural</h1>
+      <ItemListContainer name='Cami'/>
+      <Productos/>
     </>
   );
 }
---
+++ src/Components/Productos.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Productos = ( ) => {
+    const styles = {
+        color: '#520556',
+        fontFamily: 'cursive',
+        fontWeight: '15px',
+        paddingTop: '20px',
+        textAlign: 'center',
+    }
+  return (
+    <>
+        <div style={styles}>Almendra Non Parrel $3000</div>
+        <div style={styles}>Chia $550</div>
+        <div style={styles}>Arandano rojo $2500</div>
+        <div style={styles}>Nuez mariposa $2300</div>
+    </>
+  )
+}
+
+export default Productos
