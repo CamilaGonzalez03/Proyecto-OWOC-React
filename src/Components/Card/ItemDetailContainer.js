f50581173cf542c8afb9e33b7ae62db30a6c161c
Camila Gonzalez <camila.gonzalezcesar@gmail.com>
2022-10-09 16:26:06 -0300

    realizo cambios de estilo en etiquetas class

--- src/Components/Card/ItemDetailContainer.js
+++ src/Components/Card/ItemDetailContainer.js
@@ -27,7 +27,7 @@ const getItemDetail  = () => {
 
     return (
         <div className="card lg:card-side bg-base-100 shadow-xl m-10">
-            <figure><img src={item.img} alt={item.name}/></figure>
+            <figure><img src={item.img} alt={item.name} class="ml-5"/></figure>
             <div className="card-body">
                 <h2 className="card-title" class="text-orange-600 text-xl font-bold">{ item.name }</h2>
                 <p class="text-purple-700 text-xl">Precio por Kilo { item.price }</p>
